1c0975bdafeb057b2cb4062a85844aa7dc5f9373
Chesteve77 <chesteve77@gmail.com>
2022-12-16 13:56:04 -0800

    Print reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
