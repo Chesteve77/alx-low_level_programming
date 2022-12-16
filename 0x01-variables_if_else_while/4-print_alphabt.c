02c981ea6e0b06c199ab531312174d8782f078c3
Chesteve77 <chesteve77@gmail.com>
2022-12-16 00:58:18 -0800

    4Alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
