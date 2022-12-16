552daade3f769c4e6deee77531993ac4284c8309
Chesteve77 <chesteve77@gmail.com>
2022-12-16 13:52:09 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+
+	{
+		putchar(i);
+		i++;
+											}
+
+	putchar(10);
+
+	return (0);
+}
