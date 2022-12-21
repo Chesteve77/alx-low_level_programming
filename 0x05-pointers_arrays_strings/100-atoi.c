13dc5df6b132185a90322d5a6dd3ee69c0608001
Chesteve77 <chesteve77@gmail.com>
2022-12-20 19:15:59 -0800

    keygen

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,12 +17,13 @@ int _atoi(char *s)
 
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
-
+		
 		else if (num > 0)
 			break;
+	} 
 	
-	} while (*s++);
-
+	while (*s++);
+	
 	return (num * sign);
 }
 
