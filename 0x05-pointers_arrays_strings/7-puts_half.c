5d045e4f2c41fc77fe7b99837e5ad9d9e2863cac
Chesteve77 <chesteve77@gmail.com>
2022-12-20 18:43:35 -0800

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * return: void
+ */
+
+
+void puts_half(char *str)
+{
+	int i, j, max;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	max = i;
+	j = max / 2;
+	while (j <= max)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
