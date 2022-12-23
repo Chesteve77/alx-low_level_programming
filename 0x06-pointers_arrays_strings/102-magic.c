7b2e97d56992b24dcf803684b67ed64ce530bf14
Chesteve77 <chesteve77@gmail.com>
2022-12-23 03:38:51 -0800

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * program prints 98/n
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
