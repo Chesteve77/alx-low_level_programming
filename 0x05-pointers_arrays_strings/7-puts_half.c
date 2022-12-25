34bfd9a43454ea2543644708afc3af90a73c55db
Chesteve77 <chesteve77@gmail.com>
2022-12-24 16:49:41 -0800

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,26 +2,23 @@
 
 /**
  * puts_half - prints half of a string
- * @str: string
- * return: void
+ * @str: char to check
+ *
+ * Return: 0 is [success]
  */
-
-
 void puts_half(char *str)
 {
-	int i, j, max;
+	int string = 0, n;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
-	{
-		_putchar(str[j]);
-		j++;
-	}
+	while (str[string] != '\0')
+		string++;
+	if (string + 1 % 2 != '0')
+		n = (string - 1) / 2;
+	else
+		n = (string / 2);
+	n++;
+
+	for (string = n; str[string] != '\0'; string++)
+	_putchar(str[string]);
 	_putchar('\n');
 }
