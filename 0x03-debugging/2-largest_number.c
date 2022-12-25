6f5de938a2449e76d5866c1aeef7cba29720b680
Chesteve77 <chesteve77@gmail.com>
2022-12-24 16:19:23 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * largest_number -> returns the largest of e numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
+ *  largest_number - returns the largest of 3 numbers
+ *  @a: first integer
+ *  @b: second integer
+ *  @c: third integer
+ *  Return: Largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
-	if (a >= b && a >= c)
+
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b > c)
 	{
 		largest = b;
 	}
-	else if (c >= a && c >= b)
+	else
 	{
 		largest = c;
 	}
-	
 	return (largest);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int lagest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_times_table(int n);
+void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+
+#endif
