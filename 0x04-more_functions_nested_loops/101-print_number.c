a4c7728f8b15cc421feceaf8982d869d91fbb481
Chesteve77 <chesteve77@gmail.com>
2022-12-24 16:43:10 -0800

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,70 @@
 #include "main.h"
 
+
+/**
+ * power - exponents
+ *
+ * @base: base
+ *
+ * @exp: exponent
+ *
+ * Return: result (int)
+*/
+int  power(int base, int exp)
+{
+	int i, num;
+
+	num = 1;
+
+	for (i = 0; i < exp; ++i)
+		num *= base;
+	return (num);
+}
 /**
  * print_number - prints an integer
- * @n: .input integer parameter
  *
- * code by chesteve77
+ * @n: number to print
+ *
+ * Return void
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
+
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -1;
+		negative = 1;
+		n = n * -1;
 	}
-	if (i / 10)
+	while (place >= 0)
 	{
-		print_number(i / 10);
+		/*divisor = pow(10, place);*/
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
 	}
-	_putchar(i % 10 + '0');
+	if (digit == 0 && divisor == 1)
+	{
+		_putchar(48);
+	}
 }
